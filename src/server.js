f507abf10c06c2bf43a50b28f765d98b75dd55ea
n2ai <longhai2511@gmail.com>
2023-08-30 23:51:54 -0500

    update

--- src/server.js
+++ src/server.js
@@ -9,8 +9,11 @@ const hostname = process.env.HOST_NAME
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
+//config static files
+app.use(express.static(path.join(__dirname,'public')))
+
 app.get('/',(req,res)=>{
-    res.send('Hello World!')
+    res.send('Hello World! with nodemon')
 })
 
 app.get('/abc', (req,res)=>{
