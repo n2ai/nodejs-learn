4a801ec8cd17b6bb8c85cbdd7847f875069a234e
n2ai <longhai2511@gmail.com>
2023-09-04 21:38:32 -0500

    Add mySQL2

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const path = require('path')
 require('dotenv').config()
 const port = process.env.port || 8888
 const hostname = process.env.HOST_NAME
+//import mysql
+const mysql = require('mysql2')
+
 //config import
 const configViewEngine = require('./config/viewEngine')
 
@@ -17,6 +20,24 @@ configViewEngine(app)
 //routes
 //first parameter will be the prefix, the rest follow
 app.use('/', webRoutes)
+
+//test connection
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: 3307,
+    user:'root',
+    password:'123456',
+    database:'hoidanit'
+})
+
+//query
+connection.query(
+    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+    function (err,results,fields){
+        console.log(results);
+        console.log(fields)
+    }
+)
 
 app.listen(port,()=>{
     console.log(`Example app listening on ${port}`)
