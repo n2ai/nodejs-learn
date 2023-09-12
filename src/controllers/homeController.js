34530cd2e22e9f4de86b78b9c5c34b8bf859091f
n2ai <longhai2511@gmail.com>
2023-09-11 19:26:41 -0500

    Async Await

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -13,19 +13,40 @@ const getHoiDanIT = (req,res)=>{
     res.render('sample.ejs')
 }
 
-const postCreateUser = (req,res)=>{
+const getCreatePage = (req,res)=>{
+    res.render('create.ejs')
+}
+
+const postCreateUser = async (req,res)=>{
     
     let {email, myname,city} = req.body
 
-    connection.query(
-        `INSERT INTO Users (email,name,city) VALUES (?,?,?)`,
-        [email,myname,city],
-        function(err,results){
-            console.log(results)
+    // connection.query(
+    //     `INSERT INTO Users (email,name,city) VALUES (?,?,?)`,
+    //     [email,myname,city],
+    //     function(err,results){
+    //         console.log(results)
 
-            res.send('Created user succeed !')
-        }
+    //         res.send('Created user succeed !')
+    //     }
+    // )
+    
+    let [results,fields] = await connection.query(
+        `INSERT INTO Users (email,name,city) VALUES (?,?,?)`,
+        [email,myname,city]
     )
+
+    // connection.query(
+    //     'select * from Users u',
+    //     function(err,results,fields){
+    //         console.log(">>>results=", results)
+    //     }
+    // )
+
+    // const [results,fields] = await connection.query('select * from Users u',)
+
+    console.log(">>>check results: ", results)
+
 }
 
-module.exports = {getHomepage, getABC,getHoiDanIT, postCreateUser}
+module.exports = {getHomepage, getABC,getHoiDanIT, postCreateUser, getCreatePage}
--- src/routes/web.js
+++ src/routes/web.js
@@ -3,13 +3,12 @@ const router = express.Router()
 
 //router.Method('/route', handler)
 
-const {getHomepage, getABC,getHoiDanIT, postCreateUser} = require('../controllers/homeController')
+const {getHomepage, getABC,getHoiDanIT, postCreateUser, getCreatePage} = require('../controllers/homeController')
 
 router.get('/',getHomepage)
-
 router.get('/abc',getABC)
-
 router.get('/hoidanit',getHoiDanIT)
+router.get('/create',getCreatePage)
 
 router.post('/create-user', postCreateUser)
 module.exports = router
